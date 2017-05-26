9620bfa6302c9bef47a0282c981297bc544bee4e
Gonzalo Villarino <ghv8@cornell.edu>
2017-05-26 18:53:42 -0400

    Update HM_AllDays_T.F_Miguel.R

--- HM_AllDays_T.F_Miguel.R
+++ HM_AllDays_T.F_Miguel.R
@@ -13,7 +13,7 @@ library("RColorBrewer")
 # get data
 
 TF=read.table(file = "19_common_TF_AllDays.txt", header = T)
-#TF=TF[,c(-22)]
+#TF=TF[,c(-22)] # this line adds the annotation instead of gene id
 
 ####################################################################################################################
 # HEATMAPS
@@ -24,7 +24,7 @@ hmcol <- colorRampPalette(brewer.pal(9, "GnBu"))(100)
 
 #TF = unique(TF)
 all5 = TF[,2:(ncol(TF)-1)]
-rownames(all5) = TF[,1]
+rownames(all5) = TF[,1] # this line adds the annotation instead of gene id
 
 all5= as.matrix(all5)
 
