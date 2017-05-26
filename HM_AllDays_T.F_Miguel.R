09f48304c77501278f92f6791bda61a06d0aba96
Gonzalo Villarino <ghv8@cornell.edu>
2017-05-26 18:51:13 -0400

    Add files via upload

---
+++ HM_AllDays_T.F_Miguel.R
@@ -0,0 +1,33 @@
+rm(list=ls())
+setwd("~/Documents/NCSU/RNAseq_Mimulus/HeatMaps_TFonly_Miguel/")
+getwd()
+
+library ("pheatmap")
+library ("NMF")
+library("dendextend")
+library("d3heatmap")
+library("ape")
+library("gplots")
+library("RColorBrewer") 
+
+# get data
+
+TF=read.table(file = "19_common_TF_AllDays.txt", header = T)
+#TF=TF[,c(-22)]
+
+####################################################################################################################
+# HEATMAPS
+####################################################################################################################
+
+hmcol <- colorRampPalette(brewer.pal(9, "GnBu"))(100)
+
+
+#TF = unique(TF)
+all5 = TF[,2:(ncol(TF)-1)]
+rownames(all5) = TF[,1]
+
+all5= as.matrix(all5)
+
+par(oma=c(2,0,2,3))
+
+heatmap.2(all5,dendrogram="both",trace="none",scale="row",col=hmcol,labRow = TF$Family)
